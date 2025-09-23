68a469dbe252e4718fcedafb36a574cf30387924
programmerz <zamorayj20@gmail.com>
2025-09-23 14:55:20 -0500

    dni

--- BaseDeDatos.sql
+++ BaseDeDatos.sql
@@ -13,3 +13,5 @@ INSERT INTO `USUARIO` (`username`, `nombre`, `contrasena`, `correo`, `tipo_usuar
 ('yamir_zamora', 'Yamir Zamora', 'pass123', 'zamora@gmail.com', 'A', 0),
 ('sebastian_henckell', 'Sebastián Henckell', 'pass456', 'sebastian@gmail.com', 'P', 0),
 ('jeremy_garcia', 'Jeremy García', 'pass789', 'jeremy@gmail.com', 'P', 0);
+
+ALTER TABLE `USUARIO` ADD `dni` CHAR(8) NOT NULL UNIQUE;
--- main.py
+++ main.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 def obtenerConexion():
     try:
         connection = pymysql.connect(host='localhost',
-                                    port=3339,
+                                    port=3306,
                                     user='root',
                                     password='',
                                     database='bd_eduquiz',
